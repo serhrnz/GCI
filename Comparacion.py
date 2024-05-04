399fa391417466224f43f0b2625d7ceba32ed84a
serhrnz <89656547+serhrnz@users.noreply.github.com>
2024-05-03 19:40:30 -0600

    Update Comparacion.py

--- Comparacion.py
+++ Comparacion.py
@@ -123,8 +123,9 @@ grupo = []
 nombre_grupo = 'Grupo'
 def ejecutar_comparacion(b):
     # Aquí utilizas los valores actuales para llamar a comparar
-    resultado = comparar(pais, grupo, nombre_grupo)
-    print(f"Se ha generado la tabla comparativa entre {pais} y {nombre_grupo}. Para encontrarla, revise la sección 'Archivos' a la izquierda de la pantalla y refresque la ventana.")
+    resultado = comparar(pais, grupo, nombre_grupo) 
+    with salida:
+        print(f"Se ha generado la tabla comparativa entre {pais} y {nombre_grupo}. Para encontrarla, revise la sección 'Archivos' a la izquierda de la pantalla y refresque la ventana.")
 # Función para mostrar los widgets adicionales
 def mostrar_elementos(b):
     cantidad = entrada_cantidad.value
